6dd689b5e2d141e56a7288636005d59ef670961f
SiobhanMcCaffrey <siobhanamccaffrey@gmail.com>
2022-10-17 15:46:20 -0400

    commit tree b2b87bc6b88629a9ebf6d255c896c87cf8ea6d9c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
-class Dog
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/students.sqlite"
+  )
+
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+
+class Dog < ActiveRecord::Base
 end
